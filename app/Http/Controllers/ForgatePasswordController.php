eaa596c4fc8fb0a239dc551a4e0e6427e2ac0fd5
mehedi <msmbd504@gmail.com>
2022-05-26 22:36:04 +0600

    queue setup done

--- app/Http/Controllers/ForgatePasswordController.php
+++ app/Http/Controllers/ForgatePasswordController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Notifications\PasswordForgotNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class ForgatePasswordController extends Controller
 {
-    //
+    public function ForgatePassword(Request $request)
+    {
+        $validate=Validator::make($request->all(),[
+            'email'=>'required|email',
+        ]);
+
+        if ($validate->fails()){
+            return response()->json(['errors'=>$validate->errors()],422);
+        }
+
+        $email=$request->email;
+        $token=Str::random(65);
+
+        DB::table('password_resets')->insert([
+            'email'=>$email,
+            'token'=>$token,
+            'created_at'=>now()->addHours(1)
+        ]);
+        $user=User::whereEmail($email)->first();
+        //mail send
+       /* Mail::send('mail.password_reset',['token'=>$token],function ($msg) use ($email){
+            $msg->to($email);
+            $msg->subject('Password Reset');
+        });*/
+        Notification::send($user, new PasswordForgotNotification($token));
+
+        return response()->json([
+            'message'=>'Password Reset Mail Send Success.Please Check Your Email'
+        ]);
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForgatePasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::prefix('v1')->group(function (){
     Route::post('/login',[AuthController::class,'login']);
     Route::post('/register',[AuthController::class,'register']);
+    Route::post('/forgate',[ForgatePasswordController::class,'ForgatePassword']);
 
     Route::get('/login',function (){
         return response()->json('Unauthenticated');
